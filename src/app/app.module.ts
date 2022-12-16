8d777328e0ad8e7ea1f833d8dec5538816744cd4
YavuzSelimGoce <yavuz837@hotmail.com>
2022-12-16 16:43:10 +0300

    try-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { MediaComponent } from './components/media/media.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { DevicesComponent } from './components/devices/devices.component';
+import { PaymentComponent } from './components/payment/payment.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SliderComponent,
+    MediaComponent,
+    FeaturesComponent,
+    DevicesComponent,
+    PaymentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
